e29e109fbb67c7de7bc1c4ee4b5b79800c3ef6c6
Valeria Moroz <lera.posta.morozz14@gmail.com>
2021-11-03 18:05:51 +0200

    Practice 3. Created class ArrayTaskList

--- NCTaskManager/NCTaskManager/src/main/java/ua/edu/sumdu/j2se/moroz/tasks/Main.java
+++ NCTaskManager/NCTaskManager/src/main/java/ua/edu/sumdu/j2se/moroz/tasks/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		int a=5;
+		System.out.println(a);
+		System.out.println(++a);
+		System.out.println(a++);
+		System.out.println(a);
+
+		String[] arr1 = new String[5];
+		for (String i: arr1) {
+			System.out.println(i);
+		}
+		int[] arr2 = new int[12];
+
+		System.out.println(arr2.length);
+
+
 	}
 }
